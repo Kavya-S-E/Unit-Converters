2ae2fb7049bcc0d8a3b9b1ab7a7c0a3507c10d73
kavya@Kavya <kavyaeeraiah@gmail.com>
2024-04-06 20:18:35 +0530

    commit all the above files

---
+++ day2.js
@@ -0,0 +1,99 @@
+// console.log("kavya");
+// console.log("full stack developer");
+// console.log(" from besant technologies");
+
+// alert("hello");
+// prompt("enter number");
+// prompt("enter password")
+// age=21;
+// console.log("age=",age)
+
+// fullname="kavya"
+// age = 21
+// price = 99.9
+// console.log("fullname=",fullname)
+// console.log("price=",price)
+
+//  let age=30;
+//  age=67
+//  age=90
+
+// console.log("age=",age)
+
+// const age1=21;
+
+// console.log("age1=",age1)
+
+
+// const product ={
+//     title:"ball pen",price :"10",ratings :"5"
+
+// };
+// console.log("product=",product)
+// alert("hii i am kavya");
+// prompt("enter u r name")
+
+// let a=5
+// let b=3
+// console.log("a+b=",a+b);
+// console.log("a-b=",a-b)
+// console.log("a*b=",a*b)
+// console.log("a+b=",a++)
+
+// let c=3
+// let d=3
+// k=c+d
+// k++
+// console.log(k)
+
+// let c=3
+// let d=3
+// k=c+d
+// ++k
+// console.log(k)
+
+
+
+// let c=3
+// let d=3
+// k=c-d
+// k--
+// console.log(k)
+
+
+// a+=2
+// a*=3
+// a%=3
+// console.log(a)
+
+// let a=6
+// b="6"
+
+// console.log("a===b=",a===b)
+
+
+// let age =56
+// if (age >=18){
+//     console.log("can vote")
+// }
+// else{
+//     console.log("not elligible")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
